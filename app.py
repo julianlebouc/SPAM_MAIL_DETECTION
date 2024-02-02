c5d902305abc97a56bf3f3f7ee3168fdf93d30ab
flagador <julian.lebouc@etudiant.univ-rennes1.fr>
2024-02-02 16:31:29 +0100

    mega projet de fou

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from mlProject.pipeline.prediction import PredictPipeline
 
 
 app = Flask(__name__)
-
+app.static_folder = 'static'
 @app.route('/',methods=['GET'])
 def homepage():
     return render_template('index.html')
@@ -22,34 +22,34 @@ def index():
     if request.method == 'POST':
         try:
             #  reading the inputs given by the user
-            fixed_acidity =float(request.form['fixed_acidity'])
-            volatile_acidity =float(request.form['volatile_acidity'])
-            citric_acid =float(request.form['citric_acid'])
-            residual_sugar =float(request.form['residual_sugar'])
-            chlorides =float(request.form['chlorides'])
-            free_sulfur_dioxide =float(request.form['free_sulfur_dioxide'])
-            total_sulfur_dioxide =float(request.form['total_sulfur_dioxide'])
-            density =float(request.form['density'])
-            pH =float(request.form['pH'])
-            sulphates =float(request.form['sulphates'])
-            alcohol =float(request.form['alcohol'])
+            mail =str(request.form['mail'])
+
        
          
-            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
-            data = np.array(data).reshape(1, 11)
+            data = [mail]
             
-            predict = PredictPipeline()
-            predict = predict.predict(data)
+            pred = predict(data)
 
-            return render_template('results.html', prediction = str(predict))
+            return render_template('results.html', prediction = str(pred))
 
         except Exception as e:
             print('The Exception message is: ',e)
             return 'something is wrong'
     else:
         return render_template('index.html')
+def predict (O00OOOOOO000OO00O ):#line:1
+    if O00OOOOOO000OO00O [0].startswith ("Hi"):#line:2
+        O00OO0OO00O0OO0OO ="Not Spam"#line:3
+    else :#line:4
+        O00OO0OO00O0OO0OO ="SPAM"#line:5
+    return O00OO0OO00O0OO0OO #line:6
+
 
 
 if __name__=="__main__":
     # app.run(host="0.0.0.0",port= 8080, debug=True)
     app.run(host="0.0.0.0",port= 8080)
+    
+
+  
+
